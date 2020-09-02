3658681ee3e53d884ca7f2adf80844c8c5a8b3af
sousajo <josesousa@rentalcars.com>
2020-09-02 19:14:52 +0100

    test to assert the weight of three products is aaded to the total of the basket weight. closes #3

--- src/main/java/org_practice_spock/basket_example/Product.java
+++ src/main/java/org_practice_spock/basket_example/Product.java
@@ -8,6 +8,7 @@ import lombok.*;
  **/
 @Data
 @Builder
+@AllArgsConstructor
 public class Product {
     Long id;
     private String name;
--- src/test/java/basket_example_tests/BasketTest.groovy
+++ src/test/java/basket_example_tests/BasketTest.groovy
@@ -8,28 +8,14 @@ import spock.lang.Specification
 
 class BasketTest extends Specification{
 
-    @Shared
-    def tv
-
-    @Shared
-    def camera
+    @Shared def tv
+    @Shared def camera
+    @Shared def hifi
 
     void setupSpec(){
-        tv = Product.builder()
-                .name("Bravia")
-                .id(1L)
-                .price(1200)
-                .type("TV")
-                .weight(18)
-                .build()
-
-        camera = Product.builder()
-                .name("Panasonic")
-                .id(2L)
-                .price(350)
-                .type("camera")
-                .weight(2)
-                .build()
+        tv = new Product(1L,"Bravia", 1200,  18,"TV" )
+        camera = new Product(2L,"Panasonic", 350,  2,"camera" )
+        hifi = new Product(3L,"jvc", 600,  5,"HiFi" )
     }
 
     def "A Basket with one product heas equal weight"(){
@@ -51,6 +37,22 @@ class BasketTest extends Specification{
             basket.addProduct(camera)
 
         then:
-        basket.currentWeight == (tv.weight + camera.weight)
+            basket.currentWeight == (tv.weight + camera.weight)
     }
+
+    def " A basket with three products weights as their sum"(){
+        given: "Given an empty basket"
+            def basket = new Basket()
+
+        when: "User buys three products"
+            basket.addProduct(tv)
+            basket.addProduct(camera)
+            basket.addProduct(hifi)
+
+        then: "Basket weight should be the sum of the products in the basket"
+        basket.currentWeight == (tv.weight + camera.weight + hifi.weight)
+
+    }
+
+
 }
