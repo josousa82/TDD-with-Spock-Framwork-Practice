86e6a2f3aa6712814f8bb497042981924bdc55ce
sousajo <josesousa@rentalcars.com>
2020-09-02 18:28:08 +0100

    Added first test to the basket class. This test ensures the weight of the basket is the actual sum of the weight of the products

--- src/main/java/org_practice_spock/basket_example/Product.java
+++ src/main/java/org_practice_spock/basket_example/Product.java
@@ -2,8 +2,6 @@ package org_practice_spock.basket_example;
 
 import lombok.*;
 
-import java.util.function.Supplier;
-
 /**
  * Created by sousaJ on 02/09/2020
  * in package - org_practice_spock.basket_example
--- src/test/java/basket_example_tests/BasketTest.groovy
+++ src/test/java/basket_example_tests/BasketTest.groovy
@@ -1,5 +1,6 @@
 package basket_example_tests
 
+import org_practice_spock.basket_example.Basket
 import org_practice_spock.basket_example.Product
 import spock.lang.Specification
 
@@ -7,14 +8,21 @@ import spock.lang.Specification
 class BasketTest extends Specification{
 
     def "A Basket with one product heas equal weight"(){
-        given:
-        def tv = Product.builder()
-        .id(3)
-        .name("bravia")
-        .price(1200)
-        .type("tv")
-//        when:
-//        then:
-//        true
+
+        given: "An empty basket and a TV"
+            def basket = new Basket()
+            def tv = Product.builder()
+                    .name("Bravia")
+                    .id(1L)
+                    .price(1200)
+                    .type("TV")
+                    .weight(18)
+                    .build();
+
+        when: "user wants to buy the tv"
+            basket.addProduct(tv)
+
+        then:
+        basket.currentWeight == tv.weight
     }
 }
